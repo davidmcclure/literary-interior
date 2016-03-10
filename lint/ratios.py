3e278044855fe2b6cf9365657538978877b8977f
David McClure <davidwilliammcclure@gmail.com>
2016-03-10 09:36:32 -0800

    Just coalesce ratios in memory, for now.

--- lint/ratios.py
+++ lint/ratios.py
@@ -12,10 +12,10 @@ class Ratios:
     def __init__(self):
 
         """
-        Connect to Redis.
+        Initialize ratios dict.
         """
 
-        self.redis = redis.StrictRedis()
+        self.ratios = {}
 
 
     def index(self):
@@ -27,54 +27,12 @@ class Ratios:
         corpus = Corpus()
 
         for text in corpus.texts:
-
-            pipe = self.redis.pipeline()
-
-            # Register the terms.
-            pipe.sadd(
-                'lint:terms',
-                *list(text.ratios.keys()),
-            )
-
             for term, ratios in text.ratios.items():
 
-                # Append the ratios.
-                pipe.rpush(
-                    'lint:ratios:{0}'.format(term),
-                    *ratios,
-                )
+                # Update existing term.
+                if term in self.ratios:
+                    self.ratios[term] += ratios
 
-            # Batch update all words.
-            pipe.execute()
-
-
-    def ratios(self, term):
-
-        """
-        Get all ratios for a term.
-
-        Args:
-            term (str)
-
-        Returns: list<float>
-        """
-
-        ratios = self.redis.lrange(
-            'lint:ratios:{0}'.format(term),
-            0, -1,
-        )
-
-        # Cast back to floats, sort.
-        return sorted([float(r) for r in ratios])
-
-
-    @property
-    def terms(self):
-
-        """
-        Get all terms.
-
-        Returns: list
-        """
-
-        return self.redis.smembers('lint:terms')
+                # Initialize new term.
+                else:
+                    self.ratios[term] = ratios
