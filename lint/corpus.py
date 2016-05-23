eb04469a60151256afda4d9b38c5c6aa29f53426
David McClure <davidwilliammcclure@gmail.com>
2016-05-23 15:28:36 -0700

    Add the Corpus class.

---
+++ lint/corpus.py
@@ -0,0 +1,84 @@
+
+
+import scandir
+import os
+
+from lint import config
+from lint.volume import Volume
+from lint.utils import grouper
+
+
+
+class Corpus:
+
+
+    @classmethod
+    def from_env(cls):
+
+        """
+        Wrap the ENV-defined corpus root
+
+        Returns: cls
+        """
+
+        return cls(config['corpus'])
+
+
+    def __init__(self, path):
+
+        """
+        Canonicalize the corpus path.
+
+        Args:
+            path (str)
+        """
+
+        self.path = os.path.abspath(path)
+
+
+    def __len__(self):
+
+        """
+        How many volumes in the corpus?
+
+        Returns: int
+        """
+
+        return len(list(self.paths()))
+
+
+    def paths(self):
+
+        """
+        Generate asset paths.
+
+        Yields: str
+        """
+
+        for root, dirs, files in scandir.walk(self.path):
+            for name in files:
+                yield os.path.join(root, name)
+
+
+    def path_groups(self, n=1000):
+
+        """
+        Generate groups of paths.
+
+        Yields: list
+        """
+
+        for group in grouper(self.paths(), n):
+            yield group
+
+
+    def volumes(self):
+
+        """
+        Generate volume instances.
+
+        Yields: Volume
+        """
+
+        for path in self.paths():
+            yield Volume.from_path(path)
---
+++ lint/utils.py
@@ -0,0 +1,23 @@
+
+
+from itertools import islice, chain
+
+
+def grouper(iterable, size):
+
+    """
+    Yield "groups" from an iterable.
+
+    Args:
+        iterable (iter): The iterable.
+        size (int): The number of elements in each group.
+
+    Yields:
+        The next group.
+    """
+
+    source = iter(iterable)
+
+    while True:
+        group = islice(source, size)
+        yield chain([next(group)], group)
