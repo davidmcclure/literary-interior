74d3d038684e2beea0df371df8d40d02807af09e
David McClure <davidwilliammcclure@gmail.com>
2016-05-24 16:07:39 -0700

    Test that Volume#merge_offsets_for_token() merges multiple offset counts for a token under the same key.

--- test/volume/test_token_offsets.py
+++ test/volume/test_token_offsets.py
@@ -8,7 +8,6 @@ from test.helpers import make_page, make_vol
 
 
 @pytest.mark.parametrize('r', [
-    2,
     10,
     100,
     1000,
@@ -90,6 +89,82 @@ def test_index_by_offset(r):
         },
         'i': {
             round((250/300) * r): 9
+        },
+
+    }
+
+
+def test_merge_offsets_for_token():
+
+    """
+    Counts for different offsets for the same token should be merged under the
+    key for the token.
+    """
+
+    v = make_vol(pages=[
+
+        make_page(token_count=100, counts={
+            'a': {
+                'POS': 1,
+            },
+            'b': {
+                'POS': 2,
+            },
+            'c': {
+                'POS': 3,
+            },
+        }),
+
+        make_page(token_count=100, counts={
+            'b': {
+                'POS': 4,
+            },
+            'c': {
+                'POS': 5,
+            },
+            'd': {
+                'POS': 6,
+            },
+        }),
+
+        make_page(token_count=100, counts={
+            'c': {
+                'POS': 7,
+            },
+            'd': {
+                'POS': 8,
+            },
+            'e': {
+                'POS': 9,
+            },
+        }),
+
+    ])
+
+    assert v.token_offsets(1000) == {
+
+        'a': {
+            round(( 50/300) * 1000): 1,
+        },
+
+        'b': {
+            round(( 50/300) * 1000): 2,
+            round((150/300) * 1000): 4,
+        },
+
+        'c': {
+            round(( 50/300) * 1000): 3,
+            round((150/300) * 1000): 5,
+            round((250/300) * 1000): 7,
+        },
+
+        'd': {
+            round((150/300) * 1000): 6,
+            round((250/300) * 1000): 8,
+        },
+
+        'e': {
+            round((250/300) * 1000): 9,
         },
 
     }
