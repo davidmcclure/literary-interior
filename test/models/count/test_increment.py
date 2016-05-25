57563df2014281a23026c0d6bb3ccd036ce1189d
David McClure <davidwilliammcclure@gmail.com>
2016-05-25 14:45:53 -0700

    Standardize the test counts.

--- test/models/count/test_increment.py
+++ test/models/count/test_increment.py
@@ -18,15 +18,15 @@ def test_set_initial_value():
 
     cache = CountCache()
 
-    cache[1901]['token'][10] = 1
-    cache[1902]['token'][10] = 2
-    cache[1903]['token'][10] = 3
+    cache[1901]['token1'][1] = 1
+    cache[1902]['token2'][2] = 2
+    cache[1903]['token3'][3] = 3
 
     Count.increment(cache)
 
-    assert Count.token_year_offset_count('token', 1901, 10) == 1
-    assert Count.token_year_offset_count('token', 1902, 10) == 2
-    assert Count.token_year_offset_count('token', 1903, 10) == 3
+    assert Count.token_year_offset_count('token1', 1901, 1) == 1
+    assert Count.token_year_offset_count('token2', 1902, 2) == 2
+    assert Count.token_year_offset_count('token3', 1903, 3) == 3
 
 
 def test_increment_existing_value():
@@ -38,18 +38,18 @@ def test_increment_existing_value():
 
     cache = CountCache()
 
-    cache[1901]['token'][10] = 1
-    cache[1902]['token'][10] = 2
-    cache[1903]['token'][10] = 3
+    cache[1901]['token1'][1] = 1
+    cache[1902]['token2'][2] = 2
+    cache[1903]['token3'][3] = 3
 
     Count.increment(cache)
 
-    cache[1901]['token'][10] = 11
-    cache[1902]['token'][10] = 22
-    cache[1903]['token'][10] = 33
+    cache[1901]['token1'][1] = 4
+    cache[1902]['token2'][2] = 5
+    cache[1903]['token3'][3] = 6
 
     Count.increment(cache)
 
-    assert Count.token_year_offset_count('token', 1901, 10) == 1+11
-    assert Count.token_year_offset_count('token', 1902, 10) == 2+22
-    assert Count.token_year_offset_count('token', 1903, 10) == 3+33
+    assert Count.token_year_offset_count('token1', 1901, 1) == 1+4
+    assert Count.token_year_offset_count('token2', 1902, 2) == 2+5
+    assert Count.token_year_offset_count('token3', 1903, 3) == 3+6
