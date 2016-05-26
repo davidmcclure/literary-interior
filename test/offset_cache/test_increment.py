8f31c728bfdd84aa4644c707867f82afe50c3654
David McClure <davidwilliammcclure@gmail.com>
2016-05-26 13:45:39 -0700

    OffsetCache#increment() test.

--- test/offset_cache/test_increment.py
+++ test/offset_cache/test_increment.py
@@ -3,5 +3,26 @@
 from lint.offset_cache import OffsetCache
 
 
-def test_increment():
-    pass
+def test_merge_offsets():
+
+    c = OffsetCache()
+
+    c.increment(1900, {
+        'token': {
+            1:1,
+            2:2,
+            3:3,
+        }
+    })
+
+    c.increment(1900, {
+        'token': {
+            1:4,
+            2:5,
+            3:6,
+        }
+    })
+
+    assert c[1900]['token'][1] == 1+4
+    assert c[1900]['token'][2] == 2+5
+    assert c[1900]['token'][3] == 3+6
