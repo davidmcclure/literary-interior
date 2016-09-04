453d326a3f6202876126b3cd05a7343441329db7
David McClure <davidwilliammcclure@gmail.com>
2016-09-04 11:49:05 -0700

    In gather-results, don't use a separate function, since it's a one-liner.

--- bin/gather-results.py
+++ bin/gather-results.py
@@ -5,14 +5,5 @@ from lint.singletons import config
 from lint.models import Offset
 
 
-def gather_results():
-
-    """
-    Insert the offsets into the database.
-    """
-
-    Offset.gather_results(config['offsets']['htrc'])
-
-
 if __name__ == '__main__':
-    gather_results()
+    Offset.gather_results(config['offsets']['htrc'])
