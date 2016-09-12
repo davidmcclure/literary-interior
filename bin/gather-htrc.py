8aeffc6095d0d947fa84ec90c7bd87de2da0c76d
David McClure <davidwilliammcclure@gmail.com>
2016-09-12 11:47:18 -0700

    Use OffsetCache.from_results() in gather-htrc.

--- bin/gather-htrc.py
+++ bin/gather-htrc.py
@@ -3,8 +3,15 @@
 
 from lint.singletons import config, session
 from lint.models import Offset
+from lint.offset_cache import OffsetCache
 
 
 if __name__ == '__main__':
-    Offset.gather_results('htrc', config['results']['htrc'])
+
+    # Merge the pickled MPI results.
+    results = OffsetCache.from_results(config['results']['htrc'])
+
+    # Flush to SQLite.
+    Offset.flush('htrc', results)
+
     session.commit()
