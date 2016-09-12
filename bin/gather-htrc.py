7acab0ffb3468bf171a8de767a578d5cc8fb3af7
David McClure <davidwilliammcclure@gmail.com>
2016-09-12 14:31:06 -0700

    Add a Offset.gather_results() wrapper method that merges pickled caches, clears existing rows for the corpus, and flushes the new data.

--- bin/gather-htrc.py
+++ bin/gather-htrc.py
@@ -7,11 +7,4 @@ from lint.offset_cache import OffsetCache
 
 
 if __name__ == '__main__':
-
-    # Merge the pickled MPI results.
-    results = OffsetCache.from_results(config['results']['htrc'])
-
-    # Flush to SQLite.
-    Offset.insert_corpus('htrc', results)
-
-    session.commit()
+    Offset.gather_results('htrc', config['results']['htrc'])
