7ae08f2e4bdba99ac1e2bc734c5241ac5808e10b
David McClure <davidwilliammcclure@gmail.com>
2016-09-16 14:29:50 -0700

    Tests for offset_counts().

--- test/lint/utils/test_offset_counts.py
+++ test/lint/utils/test_offset_counts.py
@@ -1,5 +1,7 @@
 
 
+import pytest
+
 from lint.utils import offset_counts
 
 
@@ -7,7 +9,23 @@ def test_offset_counts():
 
     counts = offset_counts('one two three four', 4)
 
-    assert counts['one', 'CD', 0] == 1
-    assert counts['two', 'CD', 1] == 1
-    assert counts['three', 'CD', 2] == 1
-    assert counts['four', 'CD', 3] == 1
+    assert counts['one',    'CD', 0] == 1
+    assert counts['two',    'CD', 1] == 1
+    assert counts['three',  'CD', 2] == 1
+    assert counts['four',   'CD', 3] == 1
+
+
+@pytest.mark.parametrize('n', [10, 100, 1000])
+def test_round_offsets(n):
+
+    counts = offset_counts('one two three four', n)
+
+    o1 = round(0/4*n)
+    o2 = round(1/4*n)
+    o3 = round(2/4*n)
+    o4 = round(3/4*n)
+
+    assert counts['one',    'CD', o1] == 1
+    assert counts['two',    'CD', o2] == 1
+    assert counts['three',  'CD', o3] == 1
+    assert counts['four',   'CD', o4] == 1
