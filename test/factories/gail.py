d1d42dacbe9908749427776a67c3ca6cc3e4a7ca
David McClure <davidwilliammcclure@gmail.com>
2016-09-19 15:57:02 -0700

    Add GailText#xml().

--- test/factories/gail.py
+++ test/factories/gail.py
@@ -1,10 +1,13 @@
 
 
 import factory
+import pkgutil
 
 from schematics.models import Model
 from schematics.types import IntType, StringType
 from schematics.types.compound import ListType
+
+from jinja2 import Template
 
 from .schematics import SchematicsFactory
 
@@ -18,6 +21,21 @@ class GailText(Model):
     year = IntType(required=True)
 
     tokens = ListType(StringType, min_size=1)
+
+    def xml(self):
+
+        """
+        Render the XML template.
+
+        Returns: str
+        """
+
+        # Parse the template.
+        raw = pkgutil.get_data('test.factories', 'templates/gail.j2')
+        template = Template(raw.decode('utf8'))
+
+        # Render XML.
+        return template.render(**dict(self))
 
 
 class GailTextFactory(SchematicsFactory):
