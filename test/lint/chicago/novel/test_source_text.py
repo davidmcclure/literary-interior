c36245c1b14443ab36b86986dafe8a5fa8e24e76
David McClure <davidwilliammcclure@gmail.com>
2016-10-08 15:15:33 -0700

    Failing test for Gutenberg strip.

--- test/lint/chicago/novel/test_source_text.py
+++ test/lint/chicago/novel/test_source_text.py
@@ -1,4 +1,49 @@
 
 
-def test_test():
-    assert True
+from test.factories.corpora.chicago import ChicagoNovelFactory
+
+
+def test_source_text():
+
+    """
+    Return the entire text, when no Gutenberg header/footer.
+    """
+
+    text = """
+    line1
+    line2
+    line3
+    """
+
+    novel = ChicagoNovelFactory(text=text)
+
+    assert novel.source_text() == text
+
+
+def test_strip_gutenberg_boilerplate():
+
+    """
+    Strip out the Gutenberg header/footer.
+    """
+
+    text = """
+    line1
+    line2
+    line3
+    *** START OF THIS PROJECT GUTENBERG EBOOK XXX ***
+    line4
+    line5
+    line6
+    *** END OF THIS PROJECT GUTENBERG EBOOK XXX ***
+    line7
+    line8
+    line9
+    """
+
+    novel = ChicagoNovelFactory(text=text)
+
+    assert novel.source_text() == """
+    line4
+    line5
+    line6
+    """
