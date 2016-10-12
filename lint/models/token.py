ea2d1eda1347e6ab42638c27dafc6955cb084d0e
David McClure <davidwilliammcclure@gmail.com>
2016-10-11 22:22:49 -0700

    Use joblib to cache token counts.

--- lint/models/token.py
+++ lint/models/token.py
@@ -134,6 +134,28 @@ class Token(Base):
     # TODO: Shared filter method for corpus / years?
 
     @classmethod
+    def token_counts(cls, min_count=0):
+
+        """
+        Get total (un-bucketed) token counts.
+
+        Args:
+            min_count (int)
+
+        Returns: OrderedDict
+        """
+
+        query = (
+            session
+            .query(cls.token, func.sum(cls.count))
+            .group_by(cls.token)
+            .having(func.sum(cls.count) > min_count)
+            .order_by(func.sum(cls.count).desc())
+        )
+
+        return OrderedDict(query.all())
+
+    @classmethod
     def baseline_series(cls, corpus=None):
 
         """
