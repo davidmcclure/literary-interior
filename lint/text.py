50cb3dce9ad603dff2c341f34a0850547b073393
David McClure <davidwilliammcclure@gmail.com>
2016-11-03 15:47:31 -0700

    Add Text#pos_tags().

--- lint/text.py
+++ lint/text.py
@@ -7,11 +7,21 @@ from collections import namedtuple
 from nltk.tokenize import WordPunctTokenizer
 from nltk import pos_tag
 
+from textblob.utils import PUNCTUATION_REGEX
+
 
 Token = namedtuple('Token', [
     'token',
     'char1',
     'char2',
+])
+
+
+Tag = namedtuple('Tag', [
+    'token',
+    'char1',
+    'char2',
+    'pos',
 ])
 
 
@@ -37,4 +47,18 @@ class Text:
 
             for c1, c2 in tokenizer.span_tokenize(self.text)
 
+        ]
+
+    def pos_tags(self):
+
+        """
+        POS-tag the token stream.
+        """
+
+        tags = pos_tag([t.token for t in self.tokens])
+
+        return [
+            Tag(*t, pos)
+            for (token, pos), t in zip(tags, self.tokens)
+            if not PUNCTUATION_REGEX.match(token)
         ]
