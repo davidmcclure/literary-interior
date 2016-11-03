370edf298e84eff70160e9337515fc62ba2770b7
David McClure <davidwilliammcclure@gmail.com>
2016-11-03 16:01:10 -0700

    Add simple Text#snippet() method.

--- lint/text.py
+++ lint/text.py
@@ -62,3 +62,27 @@ class Text:
             for (token, pos), t in zip(tags, self.tokens)
             if not PUNCTUATION_REGEX.match(token)
         ]
+
+    def snippet(self, offset: int, padding: int=10):
+
+        """
+        Hydrate a snippet.
+        """
+
+        # prefix start
+        char1 = self.tokens[offset-padding].char1
+
+        # token start
+        char2 = self.tokens[offset].char1
+
+        # suffix start
+        char3 = self.tokens[offset].char2
+
+        # suffix end
+        char4 = self.tokens[offset+padding].char2
+
+        return (
+            self.text[char1:char2],
+            self.text[char2:char3],
+            self.text[char3:char4],
+        )
