bb39abbbb9b1f9ffc75bec0fe384130d9d8070f2
David McClure <davidwilliammcclure@gmail.com>
2016-11-06 09:35:44 -0800

    Simplify the ext-tokens test.

--- test/bin/test_ext_tokens.py
+++ test/bin/test_ext_tokens.py
@@ -19,9 +19,9 @@ def test_ext_tokens():
     ExtTokens should index token rows.
     """
 
-    t1 = TextFactory(text='one two three')
-    t2 = TextFactory(text='four five six')
-    t3 = TextFactory(text='seven eight nine')
+    t1 = TextFactory(text='t1 t2 t3')
+    t2 = TextFactory(text='t4 t5 t6')
+    t3 = TextFactory(text='t7 t8 t9')
 
     session.add(t1)
     session.add(t2)
@@ -32,32 +32,38 @@ def test_ext_tokens():
     call(['mpirun', 'bin/ext-tokens.py'])
     call(['bin/gather-tokens.py'])
 
-    assert Token.query.filter_by(
-        text=t1,
-        token='one',
-        pos='CD',
-        char1=0,
-        char2=3,
-        offset=0,
-        ratio=0/3,
-    ).count() == 1
+    for i, token in enumerate(['t1', 't2', 't3']):
 
-    assert Token.query.filter_by(
-        text=t1,
-        token='two',
-        pos='CD',
-        char1=4,
-        char2=7,
-        offset=1,
-        ratio=1/3,
-    ).count() == 1
+        assert Token.query.filter_by(
+            text=t1,
+            token=token,
+            pos='NN',
+            char1=i*3,
+            char2=i*3+2,
+            offset=i,
+            ratio=i/3,
+        ).count() == 1
 
-    assert Token.query.filter_by(
-        text=t1,
-        token='three',
-        pos='CD',
-        char1=8,
-        char2=13,
-        offset=2,
-        ratio=2/3,
-    ).count() == 1
+    for i, token in enumerate(['t4', 't5', 't6']):
+
+        assert Token.query.filter_by(
+            text=t2,
+            token=token,
+            pos='NN',
+            char1=i*3,
+            char2=i*3+2,
+            offset=i,
+            ratio=i/3,
+        ).count() == 1
+
+    for i, token in enumerate(['t7', 't8', 't9']):
+
+        assert Token.query.filter_by(
+            text=t3,
+            token=token,
+            pos='NN',
+            char1=i*3,
+            char2=i*3+2,
+            offset=i,
+            ratio=i/3,
+        ).count() == 1
