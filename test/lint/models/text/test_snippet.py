4ba25ef8198c34617e956b99e4164294345abfd8
David McClure <davidwilliammcclure@gmail.com>
2017-01-27 11:37:29 -0800

    Clear all PEP8 violations.

--- test/lint/models/text/test_snippet.py
+++ test/lint/models/text/test_snippet.py
@@ -9,9 +9,9 @@ def test_snippet():
 
     prefix, token, suffix = text.snippet(2, 5, padding=2)
 
-    assert prefix   == '01'
-    assert token    == '234'
-    assert suffix   == '56'
+    assert prefix == '01'
+    assert token == '234'
+    assert suffix == '56'
 
 
 def test_overflow():
@@ -20,6 +20,6 @@ def test_overflow():
 
     prefix, token, suffix = text.snippet(2, 5, padding=20)
 
-    assert prefix   == '01'
-    assert token    == '234'
-    assert suffix   == '56'
+    assert prefix == '01'
+    assert token == '234'
+    assert suffix == '56'
