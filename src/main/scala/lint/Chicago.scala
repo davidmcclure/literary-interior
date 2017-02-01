897456dc2f0616682054b1f748aab79db64db2b7
David McClure <davidwilliammcclure@gmail.com>
2017-02-01 13:48:30 -0800

    Sketch in Chicago corpus adapter.

--- src/main/scala/lint/Chicago.scala
+++ src/main/scala/lint/Chicago.scala
@@ -1,3 +1,51 @@
 
 
 package lint.chicago
+
+import java.io.File
+import com.github.tototoshi.csv.CSVReader
+
+
+case class Metadata(
+  identifier: Int,
+  title: String,
+  authorFirst: String,
+  authorLast: String,
+  year: Int
+)
+
+class Corpus(val metadataPath: String, textPath: String) {
+
+  // Read metadata CSV.
+  private val reader = CSVReader.open(new File(metadataPath))
+
+  // Get (id, metadata) tuples.
+  private val pairs = for (row <- reader.allWithHeaders) yield {
+
+    val md = new Metadata(
+      identifier=row("BOOK_ID").toInt,
+      title=row("TITLE"),
+      authorFirst=row("AUTH_FIRST"),
+      authorLast=row("AUTH_LAST"),
+      year=row("PUBL_DATE").toInt
+    )
+
+    (md.identifier, md)
+
+  }
+
+  // Map id -> metadata.
+  val metadata: Map[Int, Metadata] = pairs.toMap
+
+}
+
+object Chicago extends App {
+
+  val c = new Corpus(
+    "/Users/dclure/Projects/data/stacks/Chicago Corpus/NOVELS_METADATA.csv",
+    "/Users/dclure/Projects/data/stacks/Chicago Corpus/Texts"
+  )
+
+  println(c.metadata)
+
+}
