e9d32680cee726b4114f46aa779f14ea83c42b85
David McClure <davidwilliammcclure@gmail.com>
2017-02-02 08:58:54 -0800

    Install sbt-assembly, mark Spark as 'provided' to avoid duplication errors.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,6 @@ object Dependencies {
   lazy val scalaxml = "org.scala-lang.modules" % "scala-xml_2.11" % "1.0.6"
   lazy val scalacsv = "com.github.tototoshi" %% "scala-csv" % "1.3.4"
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.1"
-  lazy val spark = "org.apache.spark" %% "spark-core" % "2.1.0"
+  lazy val spark = "org.apache.spark" %% "spark-core" % "2.1.0" % "provided"
   lazy val pprint = "com.lihaoyi" %% "pprint" % "0.4.3"
 }
