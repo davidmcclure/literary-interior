4fae74ca865523b7e52c74da2bcf63cb4144c225
David McClure <davidwilliammcclure@gmail.com>
2017-02-04 20:01:04 -0800

    Implement tokenizer.

--- src/main/scala/lint/Corpus.scala
+++ src/main/scala/lint/Corpus.scala
@@ -7,36 +7,23 @@ import scala.io.Source
 import opennlp.tools.sentdetect._
 import opennlp.tools.postag._
 import opennlp.tools.tokenize._
+import pprint._
 
 
 final case class Token(
   token: String,
+  pos: String,
   start: Int,
   end: Int,
   offset: Double
 )
 
 
-case class Tokenizer(regex: String = "[a-z]+") {
-
-  /* Given a string, generate a stream of Tokens.
-   */
-  def apply(text: String): Seq[Token] = {
-
-    val matches = regex.r.findAllMatchIn(text.toLowerCase).toSeq
-
-    val length = matches.length
-
-    for ((m, i) <- matches.zipWithIndex) yield {
-      new Token(m.matched, m.start, m.end, i.toDouble / length)
-    }
-
-  }
-
-}
-
-
 object Tokenizer {
+
+  val sentenceModel = loadSentenceModel
+  val tokenizerModel = loadTokenizerModel
+  val posModel = loadPOSModel
 
   def loadSentenceModel = {
     val path = getClass.getResource("/en-sent.bin")
@@ -44,7 +31,7 @@ object Tokenizer {
     new SentenceDetectorME(model)
   }
 
-  def loadTokenzerModel = {
+  def loadTokenizerModel = {
     val path = getClass.getResource("/en-token.bin")
     val model = new TokenizerModel(path)
     new TokenizerME(model)
@@ -54,6 +41,53 @@ object Tokenizer {
     val path = getClass.getResource("/en-pos-maxent.bin")
     val model = new POSModel(path)
     new POSTaggerME(model)
+  }
+
+  /* Convert a string into a stream of Tokens.
+   */
+  def tokenize(text: String): Seq[Token] = {
+
+    // Get sentence boundaries.
+    val sentPos = sentenceModel.sentPosDetect(text)
+
+    val tokens = sentPos.flatMap(sentSpan => {
+
+      // Extract sentence text.
+      val sentence = text.slice(sentSpan.getStart, sentSpan.getEnd)
+
+      // Get token boundaries.
+      val tokenPos = tokenizerModel.tokenizePos(sentence)
+
+      // Extract tokens
+      val tokens = for (tokenSpan <- tokenPos) yield {
+        sentence.slice(tokenSpan.getStart, tokenSpan.getEnd)
+      }
+
+      // POS-tag tokens.
+      val tags = posModel.tag(tokens)
+
+      // Zip together (token, POS, start, end).
+      for (
+         (token, tag, span) <-
+         (tokens, tags, tokenPos).zipped.toList
+      ) yield {
+
+        val start = sentSpan.getStart + span.getStart
+        val end = sentSpan.getStart + span.getEnd
+
+        (token, tag, start, end)
+
+      }
+
+    })
+
+    val length = tokens.length
+
+    // Thread in the 0-1 offset.
+    for (((token, tag, start, end), i) <- tokens.zipWithIndex) yield {
+      new Token(token, tag, start, end, i.toDouble / (length-1))
+    }
+
   }
 
 }
@@ -83,9 +117,7 @@ object Text {
     text: String
   ) = {
 
-    val tokenize = new Tokenizer
-
-    val tokens = tokenize(text)
+    val tokens = Tokenizer.tokenize(text)
 
     new Text(
       identifier,
@@ -99,4 +131,10 @@ object Text {
 
   }
 
+}
+
+
+object Corpus extends App {
+  val tokens = Tokenizer.tokenize("My name is David. I love Kara.")
+  pprintln(tokens)
 }
