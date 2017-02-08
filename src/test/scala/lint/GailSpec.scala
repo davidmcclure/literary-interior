9a65b7274427e95555dcaf1b9b16d72abb007078
David McClure <davidwilliammcclure@gmail.com>
2017-02-08 12:49:09 -0800

    Stub in Gail Novel tests.

---
+++ src/test/scala/lint/GailSpec.scala
@@ -0,0 +1,14 @@
+
+
+package lint.gail
+
+import org.scalatest._
+
+
+class NovelSpec extends FlatSpec with Matchers {
+
+  "Novel.identifier()" should "provide the PSMID" in {
+    true shouldEqual true
+  }
+
+}
