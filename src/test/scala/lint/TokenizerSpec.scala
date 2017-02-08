f30a158dbcf1122895c97adb944a6f181fc7cfbd
David McClure <davidwilliammcclure@gmail.com>
2017-02-08 10:06:09 -0800

    Initial passing tests for Tokenizer.

--- src/test/scala/lint/TokenizerSpec.scala
+++ src/test/scala/lint/TokenizerSpec.scala
@@ -1,14 +1,47 @@
 
+
 package lint.tokenizer
 
 import org.scalatest._
+import pprint._
 
 
 class TokenizerSpec extends FlatSpec with Matchers {
 
-  "The test" should "work" in {
-    true shouldEqual true
+  "Tokenizer.tokenize()" should "POS-tag tokens" in {
+
+    val tokens = Tokenizer.tokenize("My name is David.")
+
+    tokens(0).token shouldEqual "my"
+    tokens(0).pos shouldEqual "PRP$"
+
+    tokens(1).token shouldEqual "name"
+    tokens(1).pos shouldEqual "NN"
+
+    tokens(2).token shouldEqual "is"
+    tokens(2).pos shouldEqual "VBZ"
+
+    tokens(3).token shouldEqual "david"
+    tokens(3).pos shouldEqual "NNP"
+
+  }
+
+  it should "store character offsets" in {
+
+    val tokens = Tokenizer.tokenize("12 34 56")
+
+    tokens(0).token shouldEqual "12"
+    tokens(0).start shouldEqual 0
+    tokens(0).end shouldEqual 2
+
+    tokens(1).token shouldEqual "34"
+    tokens(1).start shouldEqual 3
+    tokens(1).end shouldEqual 5
+
+    tokens(2).token shouldEqual "56"
+    tokens(2).start shouldEqual 6
+    tokens(2).end shouldEqual 8
+
   }
 
 }
-
