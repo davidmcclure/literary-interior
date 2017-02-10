ef275f967e36bc34b09d3e812efd3f66d9246e00
David McClure <davidwilliammcclure@gmail.com>
2017-02-10 11:11:54 -0800

    Add NovelSpec.makeNovel, to DRY up the template rendering / conversion.

--- src/test/scala/lint/gale/NovelSpec.scala
+++ src/test/scala/lint/gale/NovelSpec.scala
@@ -11,39 +11,60 @@ import com.hubspot.jinjava.Jinjava
 class NovelSpec extends FlatSpec with Matchers {
 
   ".identifier" should "provide the PSMID" in {
-    val xml = lint.xml.gale(identifier="1")
-    val novel = Novel.fromString(xml.toString.trim)
+    val novel = NovelSpec.makeNovel(identifier="1")
     novel.identifier shouldEqual "1"
   }
 
   ".title" should "provide the title" in {
-    val xml = lint.xml.gale(title="Moby Dick")
-    val novel = Novel.fromString(xml.toString.trim)
+    val novel = NovelSpec.makeNovel(title="Moby Dick")
     novel.title shouldEqual "Moby Dick"
   }
 
   ".authorFirst" should "provide the author's first name" in {
-    val xml = lint.xml.gale(authorFirst="Herman")
-    val novel = Novel.fromString(xml.toString.trim)
+    val novel = NovelSpec.makeNovel(authorFirst="Herman")
     novel.authorFirst shouldEqual "Herman"
   }
 
   ".authorLast" should "provide the author's last name" in {
-    val xml = lint.xml.gale(authorLast="Melville")
-    val novel = Novel.fromString(xml.toString.trim)
+    val novel = NovelSpec.makeNovel(authorLast="Melville")
     novel.authorLast shouldEqual "Melville"
   }
 
   ".year" should "provide the publication year" in {
-    val xml = lint.xml.gale(year=1851)
-    val novel = Novel.fromString(xml.toString.trim)
+    val novel = NovelSpec.makeNovel(year=1851)
     novel.year shouldEqual 1851
   }
 
   ".plainText" should "provide joined tokens" in {
-    val xml = lint.xml.gale(tokens=Seq("Call", "me", "Ishmael."))
-    val novel = Novel.fromString(xml.toString.trim)
+    val novel = NovelSpec.makeNovel(tokens=Seq("Call", "me", "Ishmael."))
     novel.plainText shouldEqual "Call me Ishmael."
+  }
+
+}
+
+
+object NovelSpec {
+
+  def makeNovel(
+    identifier: String = "1",
+    title: String = "title",
+    authorFirst: String = "first",
+    authorLast: String = "last",
+    year: Int = 1900,
+    tokens: Seq[String] = Seq("token")
+  ): Novel = {
+
+    val xml = lint.xml.gale(
+      identifier=identifier,
+      title=title,
+      authorFirst=authorFirst,
+      authorLast=authorLast,
+      year=year,
+      tokens=tokens
+    )
+
+    Novel.fromString(xml.toString.trim)
+
   }
 
 }
