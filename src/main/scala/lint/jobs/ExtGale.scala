9c0b4943110c68bc714d05993ab8453285af4419
David McClure <davidwilliammcclure@gmail.com>
2017-02-16 14:35:08 -0800

    Handle errors in the Spark map.

--- src/main/scala/lint/jobs/ExtGale.scala
+++ src/main/scala/lint/jobs/ExtGale.scala
@@ -3,7 +3,7 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-import scala.util.Try
+import scala.util.{Try,Success,Failure}
 
 import lint.text._
 import lint.gale._
@@ -20,12 +20,21 @@ object ExtGale {
 
     val texts = sc
       .parallelize(FileSystemLoader.listSources)
-      .map(FileSystemLoader.parse)
+      .map(s => Try(FileSystemLoader.parse(s)))
+      .filter {
+        case Success(v) => true
+        case Failure(e) => {
+          println(e)
+          false
+        }
+      }
+      .map(_.get)
 
     val ds = spark.createDataset(texts)
 
     ds.write.parquet("gale.parquet")
     ds.show()
+    println(ds.count())
 
   }
 
