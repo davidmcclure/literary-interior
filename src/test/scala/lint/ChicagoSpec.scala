96eb467dc552c1fa8214494b4ba4403c2446dc09
David McClure <davidwilliammcclure@gmail.com>
2017-02-23 17:48:03 -0800

    Test that the novels CSV parses.

---
+++ src/test/scala/lint/ChicagoSpec.scala
@@ -0,0 +1,18 @@
+
+
+package lint.chicago
+
+import org.scalatest._
+
+
+class FileSystemNovelsCSVSpec extends FreeSpec with Matchers {
+
+  val url = getClass().getResource("/fixtures/chicago/NOVELS_METADATA.csv")
+  val reader = new FileSystemNovelsCSV(url.getFile)
+
+  "CSV should parse without errors" in {
+    val rows = reader.read
+    rows.size should be > 0
+  }
+
+}
