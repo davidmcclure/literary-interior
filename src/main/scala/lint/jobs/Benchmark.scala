2eeaae611cc09d0af302ca1b05be485a1b32d74d
David McClure <davidwilliammcclure@gmail.com>
2017-02-24 12:13:29 -0800

    Add a Benchmark job.

---
+++ src/main/scala/lint/jobs/Benchmark.scala
@@ -0,0 +1,27 @@
+
+
+import org.apache.spark.{SparkContext,SparkConf}
+import pprint.pprintln
+
+
+object Benchmark {
+
+  val sc = new SparkContext(new SparkConf)
+
+  def main(args: Array[String]) {
+
+    val data = sc.parallelize(0 until 10)
+
+    val res = data
+      .map(i => {
+        new Thread
+        Thread.sleep(1000)
+        i+1
+      })
+      .collect()
+
+    pprintln(res)
+
+  }
+
+}
