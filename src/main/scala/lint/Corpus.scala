a22ed5520458b711cdf65addee8dd77680fe4c28
David McClure <davidwilliammcclure@gmail.com>
2017-03-01 10:22:13 -0800

    Add a lint.corpus module, with a Novel class that maps in from Gale / Chicago.

---
+++ src/main/scala/lint/Corpus.scala
@@ -0,0 +1,52 @@
+
+
+package lint.corpus
+
+import lint.tokenizer.Token
+
+
+case class Novel(
+  corpus: String,
+  identifier: String,
+  title: String,
+  authorFirst: Option[String],
+  authorLast: Option[String],
+  year: Int,
+  text: String,
+  tokens: Seq[Token]
+)
+
+
+object Novel {
+
+  /* Map Gale novel.
+   */
+  def fromGaleNovel(novel: lint.gale.Novel) = {
+    Novel(
+      corpus="gale",
+      identifier=novel.psmid,
+      title=novel.title,
+      authorFirst=novel.authorFirst,
+      authorLast=novel.authorLast,
+      year=novel.year,
+      text=novel.text,
+      tokens=novel.tokens
+    )
+  }
+
+  /* Map Chicago novel.
+   */
+  def fromChicagoNovel(novel: lint.chicago.Novel) = {
+    Novel(
+      corpus="chicago",
+      identifier=novel.bookId,
+      title=novel.title,
+      authorFirst=Some(novel.authFirst),
+      authorLast=Some(novel.authLast),
+      year=novel.publDate,
+      text=novel.text,
+      tokens=novel.tokens
+    )
+  }
+
+}
