6b1f84c6b7424da7f1b756f12b6a8e07f27c25ad
David McClure <davidwilliammcclure@gmail.com>
2017-03-01 16:24:16 -0800

    Handle parse errors in Gale extraction.

--- src/main/scala/lint/jobs/LoadGale.scala
+++ src/main/scala/lint/jobs/LoadGale.scala
@@ -2,6 +2,7 @@
 
 import org.apache.spark.{SparkContext,SparkConf}
 import org.apache.spark.sql.{SparkSession,SaveMode}
+import scala.util.{Try,Success,Failure}
 
 import lint.gale.Loader
 import lint.config.Config
@@ -9,6 +10,7 @@ import lint.config.Config
 
 object LoadGale extends Config {
 
+  // TODO: DRY this up?
   val sc = new SparkContext(new SparkConf)
   val spark = SparkSession.builder.getOrCreate()
   import spark.implicits._
@@ -16,8 +18,19 @@ object LoadGale extends Config {
   def main(args: Array[String]) {
 
     val novels = sc
+
+      // Parse sources.
       .parallelize(Loader.sources)
-      .map(Loader.parse)
+      .map(s => Try(Loader.parse(s)))
+
+      // Log + prune errors.
+      .filter {
+        case Success(v) => true
+        case Failure(e) => println(e); false;
+      }
+
+      // Get results.
+      .map(_.get)
 
     val ds = spark.createDataset(novels)
 
