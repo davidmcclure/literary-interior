31c2773b417edf0487888a89a70df4e1cafda6da
David McClure <davidwilliammcclure@gmail.com>
2017-03-03 10:51:18 -0800

    Move the kwic() method onto Novel.

--- src/main/scala/literaryinterior/Corpus.scala
+++ src/main/scala/literaryinterior/Corpus.scala
@@ -15,4 +15,61 @@ case class Novel(
   year: Int,
   text: String,
   tokens: Seq[Token]
-) extends NovelSchema
+) extends NovelSchema {
+
+  /* Given a query token, find all occurrences within an offset range and
+   * provide match instances.
+   */
+  def kwic(
+    query: String,
+    minOffset: Double,
+    maxOffset: Double,
+    radius: Int
+  ): Seq[KWICMatch] = {
+
+    for (
+      token <- tokens
+      if (token.token == query)
+      if (token.offset >= minOffset)
+      if (token.offset <= maxOffset)
+    ) yield {
+
+      val c1 = token.start - radius
+      val c2 = token.start
+      val c3 = token.end
+      val c4 = token.end + radius
+
+      val prefix = text.slice(c1, c2)
+      val hit = text.slice(c2, c3)
+      val suffix = text.slice(c3, c4)
+
+      val snippet = prefix + s"***${hit}***" + suffix
+
+      KWICMatch(
+        corpus=corpus,
+        identifier=identifier,
+        title=title,
+        authorFirst=authorFirst,
+        authorLast=authorLast,
+        year=year,
+        offset=token.offset,
+        snippet=snippet
+      )
+
+    }
+
+  }
+
+}
+
+
+case class KWICMatch(
+  corpus: String,
+  identifier: String,
+  title: String,
+  authorFirst: Option[String],
+  authorLast: Option[String],
+  year: Int,
+  offset: Double,
+  snippet: String
+)
