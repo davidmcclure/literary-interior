677cf99b918c80855a14f07c0dac76fc0ab01b6d
David McClure <davidwilliammcclure@gmail.com>
2017-03-03 19:17:16 -0800

    Re-add KWIC.

---
+++ src/main/scala/lint/jobs/KWIC.scala
@@ -0,0 +1,101 @@
+
+
+package lint.jobs
+
+import org.apache.spark.{SparkContext,SparkConf}
+import org.apache.spark.sql.{SparkSession,SaveMode}
+
+import lint.config.Config
+import lint.corpus.Novel
+
+
+case class KWICOpts(
+  query: String = "",
+  outPath: String = "",
+  minOffset: Double = 0,
+  maxOffset: Double = 100,
+  minYear: Double = 0,
+  maxYear: Double = 3000,
+  radius: Int = 100,
+  fraction: Double = 1
+)
+
+
+object KWIC extends Config {
+
+  val sc = new SparkContext(new SparkConf)
+  val spark = SparkSession.builder.getOrCreate()
+  import spark.implicits._
+
+  /* Probe for KWICs, dump results to CSV.
+   */
+  def main(args: Array[String]) {
+
+    // Define argument rules.
+    val parser = new scopt.OptionParser[KWICOpts]("kwic") {
+
+      arg[String]("query")
+        .action((x, c) => c.copy(query = x))
+        .text("Query token")
+
+      arg[String]("outPath")
+        .action((x, c) => c.copy(outPath = x))
+        .text("Output path")
+
+      opt[Double]("minOffset")
+        .action((x, c) => c.copy(minOffset = x))
+        .text("Minimum 0-1 offset.")
+
+      opt[Double]("maxOffset")
+        .action((x, c) => c.copy(maxOffset = x))
+        .text("Maximum 0-1 offset.")
+
+      opt[Int]("minYear")
+        .action((x, c) => c.copy(minYear = x))
+        .text("Minimum year.")
+
+      opt[Int]("maxYear")
+        .action((x, c) => c.copy(maxYear = x))
+        .text("Maximum year.")
+
+      opt[Int]("radius")
+        .action((x, c) => c.copy(radius = x))
+        .text("Snippet character radius.")
+
+      opt[Double]("fraction")
+        .action((x, c) => c.copy(fraction = x))
+        .text("Fraction of matches to sample.")
+
+    }
+
+    // Parse args.
+    val opts = parser.parse(args, KWICOpts()).get
+
+    // Read novels.
+    val novels = spark.read
+      .parquet(config.corpus.novelParquet)
+      .as[Novel]
+
+      // Filter by year.
+      .filter(n => {
+        n.year >= opts.minYear &&
+        n.year <= opts.maxYear
+      })
+
+    // Probe for query matches.
+    val matches = novels.flatMap(novel => novel.kwic(
+      opts.query,
+      opts.minOffset,
+      opts.maxOffset,
+      opts.radius
+    ))
+
+    // Sample results.
+    val sample = matches.sample(false, opts.fraction)
+
+    // Write single JSON.
+    sample.coalesce(1).write.json(opts.outPath)
+
+  }
+
+}
