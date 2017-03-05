94f377ee0a8a24d36a9a3fde0c87792664dc3ec2
David McClure <davidwilliammcclure@gmail.com>
2017-03-04 19:10:50 -0800

    In ExtBinCounts, cast results into unified Row case classes.

--- src/main/scala/lint/jobs/ExtBinCounts.scala
+++ src/main/scala/lint/jobs/ExtBinCounts.scala
@@ -6,8 +6,17 @@ import org.apache.spark.{SparkContext,SparkConf}
 import org.apache.spark.sql.{SparkSession,SaveMode}
 
 import lint.config.Config
-import lint.corpus.Novel
+import lint.corpus.{Novel,TokenBin}
 import lint.corpus.NovelImplicits._
+
+
+// TODO|dev
+case class Row(
+  token: String,
+  pos: String,
+  bin: Int,
+  count: Int
+)
 
 
 object ExtBinCounts extends Config {
@@ -24,9 +33,18 @@ object ExtBinCounts extends Config {
 
     // TODO: test
     val counts = novels
+
+      // Get list of (TokenBin, count)
       .flatMap(_.binCounts().toSeq)
-      .rdd
-      .reduceByKey(_+_)
+
+      // Sum the counts for each key across all texts.
+      .rdd.reduceByKey(_+_)
+
+      // Merge into database rows.
+      .map {
+        case (tb: TokenBin, count: Int) =>
+        Row(tb.token, tb.pos, tb.bin, count)
+      }
 
     counts.toDF.show(100)
 
