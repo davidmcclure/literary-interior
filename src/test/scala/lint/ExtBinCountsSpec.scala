f05508f1138e9bfe23f9c192368a210e5de58cac
David McClure <davidwilliammcclure@gmail.com>
2017-03-08 15:33:08 -0800

    Minimal dataset operations working in tests.

--- src/test/scala/lint/ExtBinCountsSpec.scala
+++ src/test/scala/lint/ExtBinCountsSpec.scala
@@ -4,26 +4,65 @@ package lint.jobs
 
 import org.apache.spark.{SparkContext,SparkConf}
 import org.apache.spark.sql.SparkSession
-
 import org.scalatest._
+
+import lint.tokenizer.Tokenizer
+import lint.corpus.Novel
 
 
 class ExtBinCountsMergeCountsSpec extends FlatSpec
   with Matchers with BeforeAndAfter {
 
-  var sc: SparkContext = _
+  var spark: SparkSession = _
 
   before {
-    val conf = new SparkConf().setMaster("local[*]").setAppName("test")
-    sc = new SparkContext(conf)
+
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("test")
+
+    spark = SparkSession.builder
+      .config(conf)
+      .getOrCreate()
+
   }
 
   after {
-    sc.stop
+    spark.stop
+  }
+
+  // TODO: Scala equivalent of FactoryBoy?
+  def getNovel(corpus: String, year: Int, text: String): Novel = {
+
+    val tokens = Tokenizer.tokenize(text)
+
+    Novel(
+      corpus=corpus,
+      identifier="1",
+      title="title",
+      authorFirst="first",
+      authorLast="last",
+      year=year,
+      text=text,
+      tokens=tokens
+    )
+
   }
 
   "ExtBinCounts.mergeCounts" should "index TokenBin -> count" in {
-    true shouldEqual true
+
+    // TODO: Do this once?
+    val spark_ = spark
+    import spark_.implicits._
+
+    val novels = for (i <- 0 until 10) yield {
+      getNovel("corpus1", 1910, "one two three")
+    }
+
+    val ds = spark.createDataset(novels)
+
+    ds.count shouldEqual 10
+
   }
 
 }
