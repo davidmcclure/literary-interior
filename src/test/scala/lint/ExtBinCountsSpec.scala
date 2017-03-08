34e33ce972054f45e0327f49059dc53f1017e35b
David McClure <davidwilliammcclure@gmail.com>
2017-03-08 09:31:24 -0800

    Spark testing harness that appears to work.

--- src/test/scala/lint/ExtBinCountsSpec.scala
+++ src/test/scala/lint/ExtBinCountsSpec.scala
@@ -2,10 +2,25 @@
 
 package lint.jobs
 
+import org.apache.spark.{SparkContext,SparkConf}
+import org.apache.spark.sql.SparkSession
+
 import org.scalatest._
 
 
-class ExtBinCountsMergeCountsSpec extends FlatSpec with Matchers {
+class ExtBinCountsMergeCountsSpec extends FlatSpec
+  with Matchers with BeforeAndAfter {
+
+  var sc: SparkContext = _
+
+  before {
+    val conf = new SparkConf().setMaster("local[*]").setAppName("test")
+    sc = new SparkContext(conf)
+  }
+
+  after {
+    sc.stop
+  }
 
   "ExtBinCounts.mergeCounts" should "index TokenBin -> count" in {
     true shouldEqual true
