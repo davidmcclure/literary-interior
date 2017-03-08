ae43f1b2b98ef4bc17e1bfd1cc1779e887fa762a
David McClure <davidwilliammcclure@gmail.com>
2017-03-08 08:51:08 -0800

    Stub in a test for ExtBinCounts.mergeCounts.

---
+++ src/test/scala/lint/ExtBinCountsSpec.scala
@@ -0,0 +1,14 @@
+
+
+package lint.jobs
+
+import org.scalatest._
+
+
+class ExtBinCountsMergeCountsSpec extends FlatSpec with Matchers {
+
+  "ExtBinCounts.mergeCounts" should "index TokenBin -> count" in {
+    true shouldEqual true
+  }
+
+}
