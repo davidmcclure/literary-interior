c41d3f1b8732caa6649cc42abc1a513e084820aa
David McClure <davidwilliammcclure@gmail.com>
2017-03-16 08:56:47 -0700

    Re-add the TestConfig jobs.

---
+++ src/main/scala/lint/jobs/TestConfig.scala
@@ -0,0 +1,30 @@
+
+
+package lint.jobs
+
+import org.apache.spark.{SparkContext,SparkConf}
+import pprint.pprintln
+
+import lint.config.Config
+
+
+object TestConfig extends Config {
+
+  val sc = new SparkContext(new SparkConf)
+
+  def main(args: Array[String]) {
+
+    val data = sc.parallelize(0 until 100)
+
+    val res = data
+      .map(i => {
+        pprintln(config)
+        i+1
+      })
+      .collect()
+
+    pprintln(res)
+
+  }
+
+}
