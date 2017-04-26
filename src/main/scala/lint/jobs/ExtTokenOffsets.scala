30bd3e6efb6563014ab70bfc6e59b187864c3fde
David McClure <davidwilliammcclure@gmail.com>
2017-04-26 11:18:25 -0700

    Add CLI arg parsing to ExtTokenOffsets.

--- src/main/scala/lint/jobs/ExtTokenOffsets.scala
+++ src/main/scala/lint/jobs/ExtTokenOffsets.scala
@@ -9,6 +9,12 @@ import lint.corpus.Novel
 import lint.corpus.NovelImplicits._
 
 
+case class TokenOffsetsOpts(
+  query: String = "",
+  outPath: String = ""
+)
+
+
 case class TokenOffsetsRow(
   corpus: String,
   identifier: String,
@@ -25,9 +31,29 @@ object ExtTokenOffsets extends Config {
 
   def main(args: Array[String]) {
 
+    // Parse CLI args.
+    val parser = new scopt.OptionParser[TokenOffsetsOpts]("offsets") {
+
+      arg[String]("query")
+        .action((x, c) => c.copy(query = x))
+        .text("Query token")
+
+      arg[String]("outPath")
+        .action((x, c) => c.copy(outPath = x))
+        .text("Output path")
+
+    }
+
+    val opts = parser.parse(args, TokenOffsetsOpts()).get
+
     val novels = spark.read
       .parquet(config.corpus.novelParquet)
       .as[Novel]
+
+    // Query offset sequences.
+    val offsets = ExtTokenOffsets.mergeOffsets(novels, opts.query)
+
+    offsets.write.json(opts.outPath)
 
   }
 
