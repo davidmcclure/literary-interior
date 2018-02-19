6f9fa6e413bcce7302f9cc02f9affa2c64d689f7
David McClure <davidwilliammcclure@gmail.com>
2018-02-18 22:44:38 -0500

    Use @cached_property on GaleNovelXML.

--- code/lint/sources.py
+++ code/lint/sources.py
@@ -2,6 +2,8 @@
 
 from lxml import etree
 
+from cached_property import cached_property
+
 from . import fs
 from .models import Token, GaleNovel
 
@@ -15,49 +17,54 @@ class GaleNovelXML:
     def __init__(self, tree):
         self.tree = tree
 
+    @cached_property
     def psmid(self):
         return self.tree.xpath('//PSMID/text()')[0]
 
+    @cached_property
     def full_title(self):
         return self.tree.xpath('//fullTitle/text()')[0]
 
+    @cached_property
     def author_first(self):
         return self.tree.xpath('//author/first/text()')[0]
 
+    @cached_property
     def author_middle(self):
         return self.tree.xpath('//author/middle/text()')[0]
 
+    @cached_property
     def author_last(self):
         return self.tree.xpath('//author/last/text()')[0]
 
+    @cached_property
     def language(self):
         return self.tree.xpath('//language/text()')[0]
 
+    @cached_property
     def pub_date_start(self):
         raw = self.tree.xpath('//pubDate/pubDateStart/text()')[0]
         return int(raw[:4])
 
+    @cached_property
     def ocr(self):
         return float(self.tree.xpath('//ocr/text()')[0])
 
+    @cached_property
     def text(self):
         tokens = self.tree.xpath('//page[@type="bodyPage"]//wd/text()')
         return ' '.join(tokens)
+
+    @cached_property
+    def tokens(self):
+        return Token.parse(self.text)
 
     def row(self):
         """Assemble a DF row.
 
         Returns: GaleNovel
         """
-        text = self.text()
-        tokens = Token.parse(text)
-
-        fields = dict(text=text, tokens=tokens)
-
-        fields = {**fields, **{
-            key: getattr(self, key)()
-            for key in GaleNovel.schema.names
-            if key not in fields
-        }}
-
-        return GaleNovel(**fields)
+        return GaleNovel(**{
+            name: getattr(self, name)
+            for name in GaleNovel.schema.names
+        })
