d20c059cce88974e4044f2891a1986c425ec6305
David McClure <davidwilliammcclure@gmail.com>
2018-03-15 12:20:08 -0400

    Get rid of error handling on Gale getters.

--- code/lint/sources.py
+++ code/lint/sources.py
@@ -18,51 +18,41 @@ class GaleNovelXML:
     def __init__(self, tree):
         self.tree = tree
 
-    @safe_cached_property
     def psmid(self):
-        return self.tree.find('//PSMID').text
+        return self.tree.findtext('//PSMID')
 
-    @safe_cached_property
     def full_title(self):
-        return self.tree.find('//fullTitle').text
+        return self.tree.findtext('//fullTitle')
 
-    @safe_cached_property
     def author_first(self):
-        return self.tree.find('//author/first').text
+        return self.tree.findtext('//author/first')
 
-    @safe_cached_property
     def author_middle(self):
-        return self.tree.find('//author/middle').text
+        return self.tree.findtext('//author/middle')
 
-    @safe_cached_property
     def author_last(self):
-        return self.tree.find('//author/last').text
+        return self.tree.findtext('//author/last')
 
-    @safe_cached_property
     def language(self):
-        return self.tree.find('//language').text
+        return self.tree.findtext('//language')
 
-    @safe_cached_property
     def pub_date_start(self):
-        raw = self.tree.find('//pubDate/pubDateStart').text
+        raw = self.tree.findtext('//pubDate/pubDateStart')
         return int(raw[:4])
 
-    @safe_cached_property
     def ocr(self):
-        return float(self.tree.find('//ocr').text)
+        return float(self.tree.findtext('//ocr'))
 
-    @safe_cached_property
     def raw_text(self):
         tokens = self.tree.findall('//page[@type="bodyPage"]//wd')
         return ' '.join([t.text for t in tokens if t.text])
 
-    @safe_cached_property
     def text(self):
         return Text.parse(self.raw_text)
 
     def row(self):
         return GaleNovel(**{
-            name: getattr(self, name)
+            name: getattr(self, name)()
             for name in GaleNovel.schema.names
         })
 
