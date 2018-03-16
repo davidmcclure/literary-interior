1b1655031cc6fed86fdf840e402332ac6e8d630d
David McClure <davidwilliammcclure@gmail.com>
2018-03-16 17:43:13 -0400

    Merge novels.

---
+++ code/lint/jobs/merge_novels.py
@@ -0,0 +1,51 @@
+
+
+import click
+
+from pyspark.sql.functions import lit
+
+from lint.conn import spark, sc
+from lint.models import Novel
+
+
+@click.command()
+@click.argument('gale_src', type=click.Path())
+@click.argument('chicago_src', type=click.Path())
+@click.argument('dest', type=click.Path())
+def main(gale_src, chicago_src, dest):
+    """Ingest Gale.
+    """
+    gale = spark.read.parquet(gale_src)
+    chicago = spark.read.parquet(chicago_src)
+
+    gale_unified = gale.select(
+        lit('gale').alias('corpus'),
+        gale.psmid.alias('identifier'),
+        gale.full_title.alias('title'),
+        gale.author_last,
+        gale.author_first,
+        gale.pub_date_start.alias('pub_year'),
+        gale.text,
+    )
+
+    chicago_unified = chicago.select(
+        lit('chicago').alias('corpus'),
+        chicago.book_id.alias('identifier'),
+        chicago.title,
+        chicago.auth_last.alias('author_last'),
+        chicago.auth_first.alias('author_first'),
+        chicago.publ_date.alias('pub_year'),
+        chicago.text,
+    )
+
+    rdd = sc.union([gale_unified.rdd, chicago_unified.rdd])
+
+    rdd = rdd.map(Novel.from_rdd)
+
+    novels = spark.createDataFrame(rdd, Novel.schema)
+
+    novels.write.mode('overwrite').parquet(dest)
+
+
+if __name__ == '__main__':
+    main()
