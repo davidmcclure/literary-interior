9a69f720d5a080184d88ed56926f375ae6a78e22
David McClure <davidwilliammcclure@gmail.com>
2018-03-18 11:55:37 -0400

    Merge novels tests.

--- code/tests/merge_novels/test_merge_novels.py
+++ code/tests/merge_novels/test_merge_novels.py
@@ -8,6 +8,15 @@ from tests.utils import read_yaml
 cases = read_yaml(__file__, 'cases.yml')
 
 
-# @pytest.mark.parametrize('psmid,fields', cases.items())
-def test_merge_novels(novels):
-    assert novels.count() == 10
+@pytest.mark.parametrize('fields', cases)
+def test_merge_novels(novels, fields):
+
+    row = (novels
+        .filter(novels.corpus == fields['corpus'])
+        .filter(novels.identifier == fields['identifier'])
+        .head())
+
+    for key, val in fields['metadata'].items():
+        assert getattr(row, key) == val
+
+    assert fields['text'] in row.text.raw
