6ba66c9ccd4af57392ce675d8147f7760fa52e46
David McClure <davidwilliammcclure@gmail.com>
2018-03-19 14:30:14 -0400

    Add benchmark job.

---
+++ code/lint/jobs/benchmark.py
@@ -0,0 +1,20 @@
+
+
+import time
+
+from lint.conn import spark, sc
+
+
+def work(i):
+    time.sleep(1)
+    return i + 1
+
+
+def main():
+    data = sc.parallelize(range(100))
+    result = data.map(work).collect()
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
