51ba0070755de9c0823bdc64d440300cbdb5dda4
David McClure <davidwilliammcclure@gmail.com>
2018-03-22 09:50:41 -0400

    dump_json_novels.

---
+++ code/lint/jobs/dump_json_novels.py
@@ -0,0 +1,27 @@
+
+
+import click
+
+from lint.utils import get_spark
+
+
+@click.command()
+@click.argument('src', type=click.Path())
+@click.argument('dest', type=click.Path())
+@click.option('--fraction', type=float, default=0.1)
+@click.option('--seed', type=int, default=1)
+@click.option('--partitions', type=int, default=100)
+def main(src, dest, fraction, seed, partitions):
+    """Sample novels, dump JSON.
+    """
+    sc, spark = get_spark()
+
+    novels = spark.read.parquet(src)
+
+    novels = novels.sample(False, fraction, seed).coalesce(partitions)
+
+    novels.write.mode('overwrite').json(dest)
+
+
+if __name__ == '__main__':
+    main()
