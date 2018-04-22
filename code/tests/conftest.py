5e7bddebdce37d793e5c9fe6f70ed130b33a68b2
David McClure <davidwilliammcclure@gmail.com>
2018-04-22 17:01:59 -0400

    Include Chicago author fields on merged novels.

--- code/tests/conftest.py
+++ code/tests/conftest.py
@@ -6,7 +6,13 @@ findspark.init()
 import os
 import pytest
 
-from lint.jobs import load_gale, load_chicago_novels, merge_novels
+from lint.jobs import (
+    load_gale,
+    load_chicago_novels,
+    load_chicago_authors,
+    merge_novels,
+)
+
 from lint.utils import get_spark
 
 from . import paths
@@ -38,12 +44,25 @@ def chicago_novels_df():
 
 
 @pytest.fixture(scope='module')
-def novels_df(gale_df, chicago_novels_df):
+def chicago_authors_df():
+    """Load Chicago authors.
+    """
+    load_chicago_authors.main.callback(
+        paths.CHICAGO_AUTHORS_CSV_PATH,
+        paths.CHICAGO_AUTHORS_DEST,
+    )
+
+    return spark.read.parquet(paths.CHICAGO_AUTHORS_DEST)
+
+
+@pytest.fixture(scope='module')
+def novels_df(gale_df, chicago_novels_df, chicago_authors_df):
     """Merge novels.
     """
     merge_novels.main.callback(
         paths.GALE_DEST,
         paths.CHICAGO_NOVELS_DEST,
+        paths.CHICAGO_AUTHORS_DEST,
         paths.NOVELS_DEST,
     )
 
