6ce53d403775135a8cb6b3b7b442f2e2c1a66039
David McClure <davidwilliammcclure@gmail.com>
2018-04-21 22:18:27 -0400

    load_chicago_authors job.

---
+++ code/lint/jobs/load_chicago_authors.py
@@ -0,0 +1,26 @@
+
+
+import click
+
+from lint.utils import get_spark
+from lint.sources import ChicagoAuthorMetadata
+from lint.models import ChicagoAuthor
+
+
+@click.command()
+@click.argument('src', type=click.Path())
+@click.argument('dest', type=click.Path())
+def main(src, dest):
+    """Ingest Chicago authors.
+    """
+    _, spark = get_spark()
+
+    rows = list(ChicagoAuthorMetadata.read_csv(src))
+
+    df = spark.createDataFrame(rows, ChicagoAuthor.schema)
+
+    df.write.mode('overwrite').parquet(dest)
+
+
+if __name__ == '__main__':
+    main()
