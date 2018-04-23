2ea1a0288a34ceb58384866a4519c770952be35e
David McClure <davidwilliammcclure@gmail.com>
2018-04-23 00:56:19 -0400

    Break out separate lint.udf module.

--- code/lint/jobs/ext_bin_counts.py
+++ code/lint/jobs/ext_bin_counts.py
@@ -3,41 +3,8 @@
 import numpy as np
 import click
 
-from pyspark.sql.functions import udf
-from pyspark.sql import types as T
-
-from lint.utils import get_spark, zip_bin
-
-
-COUNT_SCHEMA = T.MapType(
-    T.StringType(),
-    T.ArrayType(T.IntegerType()),
-)
-
-
-def _ext_bin_counts(vocab, bin_count=20):
-    """Count binned tokens.
-    """
-    vocab = set(vocab)
-
-    @udf(COUNT_SCHEMA)
-    def worker(tokens):
-
-        # Downcase tokens.
-        tokens = [t.lower() for t in tokens]
-
-        counts = {
-            token: [0] * bin_count
-            for token in vocab
-        }
-
-        for token, bin in zip_bin(tokens, bin_count):
-            if token in vocab:
-                counts[token][bin] += 1
-
-        return counts
-
-    return worker
+from lint.utils import get_spark
+from lint.udf import _ext_bin_counts
 
 
 @click.command()
---
+++ code/lint/udf.py
@@ -0,0 +1,37 @@
+
+
+from pyspark.sql import types as T
+from pyspark.sql.functions import udf
+
+from lint.utils import get_spark, zip_bin
+
+
+COUNT_SCHEMA = T.MapType(
+    T.StringType(),
+    T.ArrayType(T.IntegerType()),
+)
+
+
+def _ext_bin_counts(vocab, bin_count=20):
+    """Count binned tokens.
+    """
+    vocab = set(vocab)
+
+    @udf(COUNT_SCHEMA)
+    def worker(tokens):
+
+        # Downcase tokens.
+        tokens = [t.lower() for t in tokens]
+
+        counts = {
+            token: [0] * bin_count
+            for token in vocab
+        }
+
+        for token, bin in zip_bin(tokens, bin_count):
+            if token in vocab:
+                counts[token][bin] += 1
+
+        return counts
+
+    return worker
