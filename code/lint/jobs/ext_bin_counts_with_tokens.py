649b93321b5c41959cabd1ad6668530d690692e3
David McClure <davidwilliammcclure@gmail.com>
2018-06-22 09:46:16 -0400

    Bin counts + tokens.

---
+++ code/lint/jobs/ext_bin_counts_with_tokens.py
@@ -0,0 +1,53 @@
+
+
+import numpy as np
+import click
+
+from pyspark.sql.functions import size
+
+from lint.utils import get_spark, read_vocab_file
+from lint.udf import _ext_bin_counts
+
+
+@click.command()
+@click.argument('novels_src', type=click.Path())
+@click.argument('vocab_path', type=click.Path())
+@click.argument('dest', type=click.Path())
+@click.option('--bin_count', type=int, default=20)
+@click.option('--partitions', type=int, default=10)
+def main(novels_src, vocab_path, dest, bin_count, partitions):
+    """Extract per-text bin counts.
+    """
+    sc, spark = get_spark()
+
+    novels = spark.read.parquet(novels_src)
+
+    vocab = read_vocab_file(vocab_path)
+
+    # Remove un-cleaned Chicago texts.
+    novels = novels.filter(
+        (novels.chicago_clean == True) |
+        novels.chicago_clean.isNull()
+    )
+
+    ext_bin_counts = _ext_bin_counts(vocab, bin_count)
+
+    counts = ext_bin_counts(novels.text.tokens.text)
+
+    novels = (novels
+        .withColumn('counts', counts)
+        .withColumn('tokens', novels.text.tokens.text)
+        .withColumn('tags', novels.text.tokens.tag)
+        .drop(novels.text))
+
+    novels = novels.repartition(partitions)
+
+    writer = (novels.write
+        .option('compression', 'bzip2')
+        .mode('overwrite'))
+
+    writer.json(dest)
+
+
+if __name__ == '__main__':
+    main()
